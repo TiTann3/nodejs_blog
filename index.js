9d7534a11da3271c744415f8f8006ab4f402b139
Thanh Tan <89396098+TiTann3@users.noreply.github.com>
2024-05-04 10:56:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const path = require('path');
 const express = require('express')
+var morgan = require('morgan')
+const hbs = require('express-handlebars')
 const app = express()
-const port = 3000
+const port = 3300
+
+app.use(morgan('combined'))
+
+app.engine('hbs', hbs.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', './src/resources/views')
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 app.listen(port, () => {
